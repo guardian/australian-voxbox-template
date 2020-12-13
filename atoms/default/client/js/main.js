1fd8e08b6d4069fbefb1aa01c7b4d3b5b8c95110
jerseywonder <work@andyball.info>
2020-12-14 05:28:14 +1100

    Update resizer for in article use

--- atoms/default/client/js/main.js
+++ atoms/default/client/js/main.js
@@ -1,22 +1,41 @@
+/*
 var el = document.createElement('script');
 el.src = '<%= atomPath %>/app.js';
 document.body.appendChild(el);
+*/
+
+import { Voxbox } from 'shared/js/voxbox'
+
+fetch('https://interactive.guim.co.uk/docsdata/1B8xj0stauT_yD54Xi_XR6iRoCBDDRC5DqxcGDPQ_lYA.json')
+    .then(res => res.json())
+    .then(json => {
+
+    const googledoc = json.sheets;
+
+    new Voxbox(googledoc)
+
+    });
 
 
+function onElementHeightChange(elm, callback) {
+    var lastHeight = elm.clientHeight, newHeight;
+    (function run(){
+        newHeight = elm.clientHeight;
+        if( lastHeight != newHeight )
+            callback();
+        lastHeight = newHeight;
 
-setTimeout(() => {
-  if (window.resize) {  
-    const html = document.querySelector('html')
-    const body = document.querySelector('body')
-  
-    html.style.overflow = 'hidden'
-    html.style.margin = '0px'
-    html.style.padding = '0px'
-  
-    body.style.overflow = 'hidden'
-    body.style.margin = '0px'
-    body.style.padding = '0px'
-  
-  window.resize()
-  }
-},100)
+        if( elm.onElementHeightChangeTimer )
+            clearTimeout(elm.onElementHeightChangeTimer);
+
+        elm.onElementHeightChangeTimer = setTimeout(run, 250);
+    })();
+}
+
+if (window.frameElement) {
+
+    onElementHeightChange(document.body, function() {
+        window.frameElement.height = document.body.offsetHeight + 160
+    });
+
+}
